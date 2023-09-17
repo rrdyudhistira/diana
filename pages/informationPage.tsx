1653c607eb4c343b907978dee05face0542c16ba
Ryan Yudhistira <113612935+rrdyudhistira@users.noreply.github.com>
2023-09-17 20:24:04 +0700

    kalender

--- pages/informationPage.tsx
+++ pages/informationPage.tsx
@@ -39,7 +39,7 @@ function InformationPage() {
           {/* Button Ingatkan saya */}
           <div className="flex justify-center text-center pb-10">
             <a className="flex flex-row bg-choco hover:bg-blue-200 px-4 py-2 md:px-8 md:py-4 md:text-lg text-white rounded-md text-xs" href="https://calendar.google.com/calendar/u/0/r/eventedit?dates=20220611T020000Z/20220227T80000Z&text=Wedding+Annisa+%26+Rivaldi&location=Swiss+Belhotel+Bogor,+Babakan,+Bogor+Kota&details=Resepsi:%0ATanggal+dan+Waktu:+11+Jun+2022+11:00+WIB+-+13:00+WIB%0ATempat:+Swiss+Belhotel+Bogor,+Babakan,+Bogor+Kota&pli=1&sf=true" target="_blank" rel='noreferrer'>
-              <Icon icon="ant-design:calendar-filled" className="w-8 h-8"/><span className="pl-2 pt-2 font-Pacifico">Ingatkan Saya</span>
+              <Icon icon="noto:spiral-calendar" color="rgba(0, 0, 68, 0.2)" className="w-8 h-8"/><span className="pl-2 pt-2 font-Pacifico">Ingatkan Saya</span>
             </a>
           </div>
         </>
