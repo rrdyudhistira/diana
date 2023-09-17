364e4dfb6259c4ef301105234a48db899bfee5e8
Ryan Yudhistira <113612935+rrdyudhistira@users.noreply.github.com>
2023-09-17 20:52:20 +0700

    fixx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <div className="relative flex justify-center pt-2 pb-2 pl-4 m-auto bg-pastelgreen shadow-lg bg-fixed">
         © Copyright 2023 R. Ryan D. Yudistira, Order : 081818189231
-        
     </div>
     
   )
